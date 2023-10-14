6528b2e479657cd7d255039589ecb4c97efbc7cb
황시준 <ghkdtlwns987@hwangsijun-ui-MacBookPro.local>
2023-10-15 01:14:35 +0900

    README.md

--- src/main/java/com/survey/Controller/SurveyController.java
+++ src/main/java/com/survey/Controller/SurveyController.java
@@ -25,8 +25,17 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 public class SurveyController {
     private final CommandSurveyService surveyService;
     private final QuerySurveyService querySurveyService;
+
+    /**
+     * 설문을 제출하는 Controller 입니다.
+     * @param surveyRequestInfoDto
+     * @return EntityModel<ResultResponse>
+     * @throws Exception
+     * @author 황시준
+     * @since  1.0
+     */
     @PostMapping("/submit")
-    public EntityModel<ResultResponse> submit(@RequestBody @Valid SurveyRequestInfoDto surveyRequestInfoDto) throws Exception {
+    public EntityModel<ResultResponse> surveySubmit(@RequestBody @Valid SurveyRequestInfoDto surveyRequestInfoDto) throws Exception {
         SurveyDto surveyDto = surveyService.createSurvey(surveyRequestInfoDto);
         ResultResponse resultResponse = ResultResponse.of(ResultCode.CREATE_SURVEY_REQUEST_SUCCESS, surveyDto);
         EntityModel<ResultResponse> entityModel = EntityModel.of(resultResponse);
@@ -35,9 +44,17 @@ public class SurveyController {
         return entityModel;
     }
 
+    /**
+     * 설문 결과를 조회하는 Controller 입니다.
+     * @param Id
+     * @return EntityModel<ResultResponse>
+     * @throws Exception
+     * @author 황시준
+     * @since  1.0
+     */
     @GetMapping("/result/{surveyId}")
-    public EntityModel<ResultResponse> result(@PathVariable Long Id) throws Exception {
-        SurveyDto surveyDto = querySurveyService.getSurveyResultBySurveyId(Id);
+    public EntityModel<ResultResponse> surveyResult(@PathVariable Long surveyId) throws Exception {
+        SurveyDto surveyDto = querySurveyService.getSurveyResultBySurveyId(surveyId);
         ResultResponse resultResponse = ResultResponse.of(ResultCode.GET_SURVEY_REQUEST_SUCCESS, surveyDto);
 
         EntityModel<ResultResponse> entityModel = EntityModel.of(resultResponse);
